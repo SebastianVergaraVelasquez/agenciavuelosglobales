74dcf4f1a8fb30442e50b233fd659b4b1e40c5a3
SebastianVergaraVelasquez <97114145+SebastianVergaraVelasquez@users.noreply.github.com>
2024-06-26 19:14:10 -0500

    feat: :sparkles: passenger crud

--- src/main/java/com/fabiansebastianj1/passenger/adapters/out/PassengerMYSQLRepository.java
+++ src/main/java/com/fabiansebastianj1/passenger/adapters/out/PassengerMYSQLRepository.java
@@ -7,7 +7,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import com.fabiansebastianj1.documenttype.domain.models.DocumentType;
+import com.fabiansebastianj1.passenger.domain.models.Passenger;
 import com.fabiansebastianj1.passenger.infrastructure.PassengerRepository;
 
 public class PassengerMYSQLRepository implements PassengerRepository {
@@ -21,6 +24,88 @@ public class PassengerMYSQLRepository implements PassengerRepository {
         this.user = user;
         this.password = password;
     }
+
+    
+    @Override
+    public void delete(String id) {
+        try (Connection connection = DriverManager.getConnection(url, user,password)) {
+            String query = "DELETE FROM passenger WHERE nif = ?";
+                try (PreparedStatement statement = connection.prepareStatement(query)) {
+                    statement.setString(1,id);
+                    statement.executeUpdate();
+                }
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+    }
+
+    @Override
+    public Optional<Passenger> findById(String id) {
+        try (Connection connection = DriverManager.getConnection(url,user,password)){
+            String query = "SELECT * FROM passenger WHERE nif = ?";
+            try (PreparedStatement statement =connection.prepareStatement(query)){
+                statement.setString(1, id);
+            try(ResultSet resultSet = statement.executeQuery()){
+                if(resultSet.next()){
+                Passenger passenger = new Passenger(
+                    resultSet.getInt("id"),
+                    resultSet.getString("nif"),
+                    resultSet.getString("name"),
+                    resultSet.getInt("age"),
+                    resultSet.getString("seat"),
+                    resultSet.getInt("id_document_type"),
+                    resultSet.getInt("id_trip_booking_detail")
+                );
+                return Optional.of(passenger);
+                }
+            }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+
+    @Override
+    public void save(Passenger passenger) {
+        try (Connection connection = DriverManager.getConnection(url,user,password)){
+            String query = "INSERT INTO passenger (nif,name,age,seat,id_document_type,id_trip_booking_detail) VALUES (?,?,?,?,?,?)";
+            try (PreparedStatement statement = connection.prepareStatement(query)){
+                statement.setString(1,passenger.getNif());
+                statement.setString(2,passenger.getName());
+                statement.setInt(3,passenger.getAge());
+                statement.setString(4,passenger.getSeat());
+                statement.setInt(5,passenger.getDocumentTypeId());
+                statement.setInt(6,passenger.getTripBookingDetailId());       
+                statement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void update(Passenger passenger) {
+        try (Connection connection = DriverManager.getConnection(url,user,password)){
+            String query = "UPDATE passenger SET nif = ?,name=?,age=?,seat=?,id_document_type=?,id_trip_booking_detail=? WHERE nif = ?";
+            try (PreparedStatement statement = connection.prepareStatement(query)){
+                statement.setString(1,passenger.getNif());
+                statement.setString(1,passenger.getName());
+                statement.setInt(1,passenger.getAge());
+                statement.setString(1,passenger.getSeat());
+                statement.setInt(1,passenger.getDocumentTypeId());
+                statement.setInt(1,passenger.getTripBookingDetailId());
+                statement.setString(1,passenger.getNif());
+                statement.executeUpdate();
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+
     @Override
     public List<String> getOccupiedSeats(int tripId) {
         List<String> occupiedSeats = new ArrayList<>();
--- src/main/java/com/fabiansebastianj1/passenger/infrastructure/PassengerRepository.java
+++ src/main/java/com/fabiansebastianj1/passenger/infrastructure/PassengerRepository.java
@@ -1,8 +1,15 @@
 package com.fabiansebastianj1.passenger.infrastructure;
 
 import java.util.List;
+import java.util.Optional;
+
+import com.fabiansebastianj1.passenger.domain.models.Passenger;
 
 public interface PassengerRepository {
     List<String> getOccupiedSeats(int tripId);
     int getTotalOccupiedSeats(int tripId);
+    void save(Passenger passenger);
+    void update(Passenger passenger);
+    Optional<Passenger> findById(String id);
+    void delete(String id);
 }
